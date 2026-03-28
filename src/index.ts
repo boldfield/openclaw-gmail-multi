aaef8f8c369155a08cdde41cba47d459ff56c6d1
nightshift <brian@oldfield.io>
2026-03-28 21:54:11 +0000

    Scaffold plugin package with manifest and entry point

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { definePluginEntry } from "openclaw/plugin-sdk/plugin-entry";
+
+export default definePluginEntry(async (api) => {
+  api.log.info("openclaw-gmail-multi: initializing");
+});
